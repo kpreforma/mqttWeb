c70ff2b677d7c74f3799eb0370b83cf8f3cc78b9
Kenneth Reforma <kreforma@myseneca.ca>
2023-07-20 22:48:34 -0400

    publish to mosquitto test

--- publish.py
+++ publish.py
@@ -1,13 +1,15 @@
 import paho.mqtt.client as mqtt
-import time
+import time, datetime, json
+import random as r
 
 # MQTT information
-hostname = "localhost"
+hostname = "test.mosquitto.org"
 PORT = 1883
 topic = "test/tool1"
 
 # Create a MQTT client
 client = mqtt.Client("sender")
+
 
 # Callback functions
 def on_connect(client, userdata, flags, rc):
@@ -16,8 +18,10 @@ def on_connect(client, userdata, flags, rc):
     else:
         print(f"Failed to connect. Return code: {rc}")
 
+
 def on_publish(client, userdata, mid):
     print("Message published")
+
 
 # Set the callback functions
 client.on_connect = on_connect
@@ -32,16 +36,16 @@ client.loop_start()
 try:
     while True:
         # Generate some data to send
-        data = "Hello, MQTT!"  # Replace with your own data to be sent
-
-        # Publish the data to the specified topic
+        data = json.dumps(
+            {
+                "device_code": "tool1",
+                "status": r.randint(0, 1),
+                "datetime": str(datetime.datetime.now()),
+            }
+        )
         client.publish(topic, data)
-
-        # Print the sent data for demonstration purposes
         print(f"Sent: {data}")
-
-        # Wait for a short duration before publishing the next message
-        time.sleep(5)  # Change the delay as per your requirement
+        time.sleep(2)  # Change the delay as per your requirement
 
 except KeyboardInterrupt:
     print("Disconnecting from the broker...")
